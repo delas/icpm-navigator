bbf2d59bcd362e4369e956eaf4c92ae3e7731733
Andrea Burattin <andrea.burattin@gmail.com>
2023-11-11 15:43:59 +0100

    First test

---
+++ src/views/TabHome.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+        <ion-buttons slot="end">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-menu side="end" contentId="main-content">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Account</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+        <p>You are currently logged as test@email.com.</p>
+        <p>To change your configuration use the setting button below.</p>
+        <ion-list>
+          <ion-item>
+            <ion-icon :icon="settingsOutline" slot="start" />
+            <ion-label>Settings</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon :icon="logOutOutline" slot="start" />
+            <ion-label>Logout</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
+
+    <ion-content id="main-content" :fullscreen="true">
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon} from '@ionic/vue';
+import { settingsOutline, logOutOutline } from "ionicons/icons";
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,24 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="home" href="/tabs/home">
+          <ion-icon aria-hidden="true" :icon="home" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="calendar" href="/tabs/calendar">
+          <ion-icon aria-hidden="true" :icon="calendar" />
+          <ion-label>Agenda</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="attendees" href="/tabs/attendees">
+          <ion-icon aria-hidden="true" :icon="people" />
+          <ion-label>Attendees</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="images" href="/tabs/images">
+          <ion-icon aria-hidden="true" :icon="images" />
+          <ion-label>Gallery</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +29,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {ellipse, square, triangle, home, calendar, people, images} from 'ionicons/icons';
 </script>
