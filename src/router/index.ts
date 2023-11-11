aac4570523a55800dafa90848b53759694c744fa
Andrea Burattin <andrea.burattin@gmail.com>
2023-11-11 16:04:23 +0100

    Fixed base path

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,7 +36,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/icpm-navigator/"),
   routes
 })
 
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports  = {
+    publicPath: "/icpm-navigator/"
+}
