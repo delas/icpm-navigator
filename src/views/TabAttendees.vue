563d1d90d920ba0a9952966f37cfdf6d222818cb
Andrea Burattin <andrea.burattin@gmail.com>
2024-02-04 15:52:16 +0100

    Added company, country, and random avatar to attendees

--- src/views/TabAttendees.vue
+++ src/views/TabAttendees.vue
@@ -4,13 +4,16 @@
     <ion-content :fullscreen="true">
       <p class="ion-padding">Here is the list of all attendees:</p>
       <ion-list lines="full">
-        <ion-item v-for="person in persons" :key="person.name">
+        <ion-item v-for="(person, index) in persons" :key="person.name">
           <ion-avatar aria-hidden="true" slot="start">
-            <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+            <img alt="" :src="person.avatar + '?' + index" />
           </ion-avatar>
           <ion-label>
             <h2>{{ person.name }}</h2>
-            <p>{{ person.email }}</p>
+            <p>
+              {{ person.email }}<br>
+              {{ person.company }}, {{ person.country }}
+            </p>
           </ion-label>
         </ion-item>
       </ion-list>
@@ -28,60 +31,39 @@ export default {
   data() {
     return {
       persons: [
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
-        { name: 'John Doe', email: 'johndoe@gmail.com' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com' },
-        { name: 'John Smith', email: 'johnsmith@gmail.com' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
+        { name: 'John Doe', email: 'johndoe@gmail.com', company: 'Technical University of Denmark', country: 'Denmark', avatar: 'https://picsum.photos/100' },
+        { name: 'Jane Doe', email: 'janedoe@gmail.com', company: 'SAP', country: 'The Netherlands', avatar: 'https://picsum.photos/100' },
+        { name: 'John Smith', email: 'johnsmith@gmail.com', company: 'Celonis', country: 'Germany', avatar: 'https://picsum.photos/100' },
       ]
     };
   },
