11121ce821030df4176378e04ffd2d1fe5adb38f
Andrea Burattin <andrea.burattin@gmail.com>
2024-02-06 09:12:12 +0100

    Improved agenda

--- src/views/TabAgenda.vue
+++ src/views/TabAgenda.vue
@@ -15,19 +15,23 @@
     <ion-content id="main-content">
       <ion-list>
         <ion-item-group v-for="(session, sindex) in sessions" v-bind:key="sindex">
-          <ion-item-divider sticky>
-            <ion-label>{{ session.time }}</ion-label>
+          <ion-item-divider color="light" sticky>
+            <ion-label>
+              <h2>{{ session.time }}</h2>
+              <p v-if="session.line2">{{ session.line2 }}</p>
+              <p v-if="session.line3">{{ session.line3 }}</p>
+            </ion-label>
           </ion-item-divider>
           <ion-item-sliding>
             <ion-item v-for="(subsession, index) in session.subsessions" v-bind:key="index">
               <ion-icon slot="end" v-if="subsession.icon == 'coffee'" :icon="icons.cafeOutline" />
               <ion-icon slot="end" v-if="subsession.icon == 'registration'" :icon="icons.idCardOutline" />
               <ion-icon slot="end" v-if="subsession.icon == 'lunch'" :icon="icons.fastFoodOutline" />
-              <ion-icon slot="end" v-if="subsession.icon == 'star'" :icon="icons.starOutline" />
+              <ion-icon slot="end" v-if="subsession.icon == 'alert'" :icon="icons.alertCircleOutline" />
               <ion-label>
                 <h3 class="ion-text-wrap" v-html="subsession.name"></h3>
-                <p v-if="subsession.time">{{ subsession.time }}</p>
-                <p>{{ subsession.place }}</p>
+                <p class="ion-text-wrap" v-if="subsession.line2">{{ subsession.line2 }}</p>
+                <p class="ion-text-wrap" v-if="subsession.line3">{{ subsession.line3 }}</p>
               </ion-label>
             </ion-item>
           </ion-item-sliding>
@@ -54,7 +58,6 @@ import {
   IonAvatar
 } from '@ionic/vue';
 import * as icons from 'ionicons/icons'
-// import { idCardOutline, cafeOutline } from 'ionicons/icons'
 import HeaderBar from "@/components/HeaderBar.vue";
 </script>
 
@@ -66,61 +69,73 @@ export default {
         {
           time: '08:00',
           subsessions: [
-            {name: 'Registration', place: 'Building 116 - Foyer', icon: 'registration'},
+            {name: 'Registration', line2: 'Building 116 - Foyer', icon: 'registration'},
           ]
         },
         {
           time: '09:00 - 10:30',
           subsessions: [
-            {name: 'COMINDS', place: 'Building 116 - Workshop Room 1'},
-            {name: 'EdbA', place: 'Building 116 - Workshop Room 2'},
-            {name: 'EduPM', place: 'Building 116 - Workshop Room 3'},
-            {name: 'ML4PM', place: 'Building 116 - Workshop Room 4'},
-            {name: 'PODS4H', place: 'Building 116 - Workshop Room 5'}
+            {name: 'COMINDS', line2: 'Building 116 - Workshop Room 1'},
+            {name: 'EdbA', line2: 'Building 116 - Workshop Room 2'},
+            {name: 'EduPM', line2: 'Building 116 - Workshop Room 3'},
+            {name: 'ML4PM', line2: 'Building 116 - Workshop Room 4'},
+            {name: 'PODS4H', line2: 'Building 116 - Workshop Room 5'}
           ]
         },
         {
           time: '10:30 – 11:15',
           subsessions: [
-            {name: 'Coffee break', place: 'Building 116 - Foyer', icon: 'coffee'},
+            {name: 'Coffee break', line2: 'Building 116 - Foyer', icon: 'coffee'},
           ]
         },
         {
           time: '10:30 - 12:30',
+          line2: 'Chair: Name Surname',
+          line3: 'Building 116 - Main Auditorium',
           subsessions: [
-            {name: '<strong>Keynote</strong><br>Title: Everything there is to Know about Stochastically Known Logs', place: 'Building 116 - Main Auditorium', icon: 'star'},
+            {name: '<strong>Keynote</strong><br>Everything there is to Know about Stochastically Known Logs', line2: 'Avi Gal', icon: 'alert'},
           ]
         },
         {
           time: '12:30 – 14:00',
           subsessions: [
-            {name: 'Lunch', time: '12:30 – 14:00', place: 'Building 101 - Glass Salen', icon: 'lunch'},
+            {name: 'Lunch', line2: '12:30 – 14:00', line3: 'Building 101 - Glass Salen', icon: 'lunch'},
           ]
         },
         {
-          time: '14:00 - 15:30',
+          time: '14:00 - 15:30 - AI in Process Mining',
+          line2: 'Session chair: Chiara Di Francescomarino',
+          line3: 'Building 116 - Main Auditorium',
           subsessions: [
-            {name: 'COMINDS', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 1'},
-            {name: 'EdbA', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 2'},
-            {name: 'EduPM', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 3'},
-            {name: 'ML4PM', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 4'},
-            {name: 'PODS4H', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 5'}
+            {
+              name: 'Data-Driven Goal Recognition in Transhumeral Prostheses Using Process Mining Techniques',
+              line2: 'Zihang Su, Tianshi Yu, Nir Lipovetzky, Alireza Mohammadi, Denny Oetomo, Artem Polyvyanyy, Sebastian Sardina, Ying Tan and Nick van Beest',
+            },
+            {
+              name: 'Plan Recognition as Probabilistic Trace Alignment',
+              line2: 'Jonghyeon Ko, Fabrizio Maggi, Marco Montali, Rafael Peñaloza and Ramon Fraga Pereira',
+            },
           ]
         },
         {
           time: '15:30 – 16:00',
           subsessions: [
-            {name: 'Coffee break', time: '10:30 – 11:15', place: 'Building 116 - Foyer', icon: 'coffee'},
+            {name: 'Coffee break', line2: 'Building 116 - Foyer', icon: 'coffee'},
           ]
         },
         {
-          time: '16:00 - 17:30',
+          time: '14:00 - 15:30 - AI in Process Mining',
+          line2: 'Session chair: Chiara Di Francescomarino',
+          line3: 'Building 116 - Main Auditorium',
           subsessions: [
-            {name: 'COMINDS', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 1'},
-            {name: 'EdbA', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 2'},
-            {name: 'EduPM', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 3'},
-            {name: 'ML4PM', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 4'},
-            {name: 'PODS4H', time: '09:00 - 10:30', place: 'Building 116 - Workshop Room 5'}
+            {
+              name: 'Data-Driven Goal Recognition in Transhumeral Prostheses Using Process Mining Techniques',
+              line2: 'Zihang Su, Tianshi Yu, Nir Lipovetzky, Alireza Mohammadi, Denny Oetomo, Artem Polyvyanyy, Sebastian Sardina, Ying Tan and Nick van Beest',
+            },
+            {
+              name: 'Plan Recognition as Probabilistic Trace Alignment',
+              line2: 'Jonghyeon Ko, Fabrizio Maggi, Marco Montali, Rafael Peñaloza and Ramon Fraga Pereira',
+            },
           ]
         },
       ]
